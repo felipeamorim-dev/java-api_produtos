56677acd049ff9ac9115feae535d60370202b44a
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-10-24 22:02:42 -0300

    Projeto Criado

---
+++ src/main/java/com/solutionsfa/api_product/ApiProductApplication.java
@@ -0,0 +1,13 @@
+package com.solutionsfa.api_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutionsfa/api_product/ApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutionsfa.api_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
