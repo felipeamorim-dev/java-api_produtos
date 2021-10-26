116ca3f76f4086a100b576812bcdf6f939884783
Felipe Amorim <felipeamorim.dev@gmail.com>
2021-10-26 20:06:43 -0300

    Método Insert adicionado

--- src/main/java/com/solutionsfa/api_product/resource/UserResource.java
+++ src/main/java/com/solutionsfa/api_product/resource/UserResource.java
@@ -1,13 +1,17 @@
 package com.solutionsfa.api_product.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.solutionsfa.api_product.entity.User;
 import com.solutionsfa.api_product.services.UserService;
@@ -30,5 +34,25 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	/*
+	 *Para inserir dados no banco de dados corretamente é setado o novo usuário para camada de serviço atráves do método insert
+	 * da class UserService instanciado como service na camada de controle. Para disparar a resposta do método insert do controlador
+	 * rest será necessário utilizar o método created da classe ResponseEntity do controlador rest. Esse método solicita uma URI
+	 * para encaminhar o obj inserido no corpo da requisição pelo método body.
+	 * 
+	 * Essa forma deve ser utilizada para que o retorno da requisição possua o valor adequado para operaões de inserção atráves
+	 * da requisição realizada pelo método Post.
+	 * */
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
  }
